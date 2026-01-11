5020919b28f4b17d1f66c01c8bd464ab987fd1d3
Raghad <108527533+RaghadJohani0@users.noreply.github.com>
2026-01-11 20:14:04 +0300

    Create checkpoint.py

---
+++ src/utils/checkpoint.py
@@ -0,0 +1,10 @@
+import torch
+import os
+
+def save_checkpoint(model, optimizer, epoch, path):
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    torch.save({
+        "epoch": epoch,
+        "model_state": model.state_dict(),
+        "optimizer_state": optimizer.state_dict()
+    }, path)
